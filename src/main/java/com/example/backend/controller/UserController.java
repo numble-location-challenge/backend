85b6b047c7f73536891e7f4005176fa717783893
ulzcq <syj9913@ynu.ac.kr>
2022-11-09 23:04:24 +0900

    rename: DTO -> RequestDTO and fix join method

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -3,12 +3,12 @@ package com.example.backend.controller;
 import com.example.backend.dto.*;
 import com.example.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "user", description = "회원 API")
@@ -18,6 +18,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "회원가입")
     @PostMapping("/join")
     @ApiResponses({
@@ -25,10 +26,9 @@ public class UserController {
             @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
     })
-    public ResponseEntity<ResponseDTO> join(@RequestBody final UserJoinDTO userJoinDTO){
+    public ResponseDTO<?> join(@RequestBody final UserJoinRequestDTO userJoinRequestDTO){
         //TODO validation 처리
-        userService.create(userJoinDTO);
-        return new ResponseEntity(ResponseDTO.builder().success(true).message("회원가입 처리되었습니다.").build(), HttpStatus.CREATED);
+        userService.create(userJoinRequestDTO);
+        return ResponseDTO.builder().success(true).message("회원가입 처리되었습니다.").build();
     }
-
 }
