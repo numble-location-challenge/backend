868fc877d84dc49709c1d9390750a660713984ea
ulzcq <syj9913@ynu.ac.kr>
2022-11-10 01:39:43 +0900

    feat: add modifyUser, getUserInfo controller method

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.domain.User;
 import com.example.backend.dto.*;
 import com.example.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "user", description = "회원 API")
 @RestController
@@ -47,6 +50,61 @@ public class UserController {
 
         userService.delete(id);
         return ResponseDTO.builder().success(true).message("정상 탈퇴되었습니다.").build();
+    }
+
+    //TODO 서비스 구현, jwt 적용 전
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "회원 수정")
+    @PutMapping("/user/me/{id}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED"),
+            @ApiResponse(responseCode = "403", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    public ResponseDTO<UserProfileDTO> modifyUser(
+            @Parameter(required = true) @PathVariable Long id,
+            @RequestBody final UserModifyRequestDTO userModifyRequestDTO){
+
+        User user = userService.modifyUser(id, userModifyRequestDTO);
+        //set data list
+        List<UserProfileDTO> users = List.of(UserProfileDTO.builder()
+                .nickname(user.getNickname())
+                .profile(user.getProfile())
+                .region(user.getRegion())
+                .bio(user.getBio())
+                .build());
+
+        return ResponseDTO.<UserProfileDTO>builder().success(true).message("정상 수정 처리 되었습니다.")
+                .data(users).build();
+    }
+
+    //TODO 서비스 구현, jwt 적용 전
+    //내 프로필 조회할 때 피드, 댓글도 같이 들고 와야하는지? 일단 빼고 처리
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "회원 프로필 조회")
+    @GetMapping("/user/me/{id}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED"),
+            @ApiResponse(responseCode = "403", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    public ResponseDTO<UserProfileDTO> getUserInfo(@Parameter(required = true) @PathVariable Long id){
+
+        User user = userService.getUser(id);
+        //set data list
+        List<UserProfileDTO> users = List.of(UserProfileDTO.builder()
+                .nickname(user.getNickname())
+                .profile(user.getProfile())
+                .region(user.getRegion())
+                .bio(user.getBio())
+                .build());
+
+        return ResponseDTO.<UserProfileDTO>builder().success(true).message("프로필 조회 결과")
+                .data(users).build();
     }
 
 }
