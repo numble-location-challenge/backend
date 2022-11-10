b5ce0859668498a9efb6d07351e5e10be4451237
Dsys1129 <dsys1129@gmail.com>
2022-11-10 15:35:59 +0900

    feat: Modify Entity Feed, PostImage, Comment, TimeAuditingEntity

--- src/main/java/com/example/backend/domain/TimeAuditingEntity.java
+++ src/main/java/com/example/backend/domain/TimeAuditingEntity.java
@@ -1,13 +1,15 @@
 package com.example.backend.domain;
 
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
--- src/main/java/com/example/backend/domain/post/Feed.java
+++ src/main/java/com/example/backend/domain/post/Feed.java
@@ -1,22 +1,39 @@
 package com.example.backend.domain.post;
 
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.example.backend.domain.Comment;
+import com.example.backend.domain.PostImage;
+import com.example.backend.domain.User;
+
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Getter
 @Entity
 @Table(name = "FEED")
 @DiscriminatorValue("F")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Feed extends Post {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "social_id")
     private Social social; //작성자가 참여 중인 모임 연결
 
+    @Builder
+    public Feed(User user, List<PostImage> images,
+        List<Comment> comments, String contents, Integer region, int likes,
+        Social social) {
+        super(user, images, comments, contents, region, likes);
+        this.social = social;
+    }
 }
