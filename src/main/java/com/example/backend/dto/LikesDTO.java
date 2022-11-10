1484ed4c0fe09ed25b5f30679bf1fb5e86f939fc
HongGuma <hongsu9849@gmail.com>
2022-11-11 00:08:53 +0900

    feat: Create Likes DTO,Service,Repository

---
+++ src/main/java/com/example/backend/dto/LikesDTO.java
@@ -0,0 +1,22 @@
+package com.example.backend.dto;
+
+import com.example.backend.domain.User;
+import com.example.backend.domain.post.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LikesDTO {
+
+    @Schema(description = "좋아요 아이디")
+    private Long id;
+
+    @Schema(description = "좋아요 누른 회원 아이디")
+    private User user; //좋아요 누른 회원
+
+    @Schema(description = "좋아요 누른 게시글 아이디")
+    private Post post; //좋아요 단 글
+
+}
