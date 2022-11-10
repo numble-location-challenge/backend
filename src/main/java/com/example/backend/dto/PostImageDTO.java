ca8d26c39975ab10226a632a7e17cd4369ec3303
Dsys1129 <dsys1129@gmail.com>
2022-11-10 15:39:07 +0900

    feat: add FeedListResponseDTO, FeedRequestDTO, PostImageDTO

---
+++ src/main/java/com/example/backend/dto/PostImageDTO.java
@@ -0,0 +1,19 @@
+package com.example.backend.dto;
+
+import com.example.backend.domain.PostImage;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "이미지 저장 경로")
+@Getter
+@NoArgsConstructor
+public class PostImageDTO {
+
+    private String imagePath;
+
+    public PostImageDTO(PostImage postImage) {
+        this.imagePath = postImage.getImagePath();
+    }
+}
