fc411724ccd2fd365c791d24b801aea0fd8513cc
ulzcq <syj9913@ynu.ac.kr>
2022-11-11 20:12:06 +0900

    fix: bug due to overlapped method and rename service method

--- src/main/java/com/example/backend/service/SocialingServiceImpl.java
+++ src/main/java/com/example/backend/service/SocialingServiceImpl.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class SocialingServiceImpl implements SocialingService {
     @Override
-    public void addApplyUser(Long userId, Long socialId) {
+    public void addParticipants(Long userId, Long socialId) {
 
     }
 
     @Override
-    public void deleteUserApplied(Long userId, Long socialId) {
+    public void deleteParticipants(Long userId, Long socialId) {
 
     }
 
     @Override
-    public void dropSocialUser(Long userId, Long socialId, Long droppedUserId) {
+    public void kickOutParticipants(Long userId, Long socialId, Long droppedUserId) {
 
     }
 }
