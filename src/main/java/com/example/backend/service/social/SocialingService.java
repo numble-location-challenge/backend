35158b6a5f4227b67d529499a59f44d4d9ed2131
ulzcq <syj9913@ynu.ac.kr>
2022-11-17 14:31:32 +0900

    remove: SocialingService and SocialingServiceImpl

--- src/main/java/com/example/backend/service/social/SocialingService.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.service.social;
-
-public interface SocialingService {
-    void addParticipants(String userId, Long socialId);
-
-    void deleteParticipants(String userId, Long socialId);
-
-    void kickOutParticipants(String userId, Long socialId, Long droppedUserId);
-}
