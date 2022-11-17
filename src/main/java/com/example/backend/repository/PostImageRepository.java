7b1e1bbb938fd671672318f42ce8578b7c0271f2
ulzcq <syj9913@ynu.ac.kr>
2022-11-17 23:12:08 +0900

    feat: add TagRepository and fix createSocial, updateSocial in SocialService

--- src/main/java/com/example/backend/repository/PostImageRepository.java
+++ src/main/java/com/example/backend/repository/PostImageRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.domain.PostImage;
 
 public interface PostImageRepository extends JpaRepository<PostImage,Long> {
+
+    void deleteAllByPostId(Long postId);
 }
