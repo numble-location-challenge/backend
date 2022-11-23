c495a78ac8068f038aae9dcea7ff7e36535e29e4
ulzcq <syj9913@ynu.ac.kr>
2022-11-24 01:12:43 +0900

    fix: allowOrigins in WebMvcConfig
    클라이언트 포트 모든 Origin 허용으로 변경

--- src/main/java/com/example/backend/global/config/WebMvcConfig.java
+++ src/main/java/com/example/backend/global/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         //모든 경로에 대해
         registry.addMapping("/**")
                 //클라이언트 포트 설정
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 //메서드 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*") //모든 헤더 허용
