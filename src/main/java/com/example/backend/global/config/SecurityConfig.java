7b8a7e5d12d176ad36ac1316e313aa922d9bfeeb
ulzcq <syj9913@ynu.ac.kr>
2022-11-24 01:26:32 +0900

    feat: add JwtLogoutHandler and JwtExceptionEntryPoint

--- src/main/java/com/example/backend/global/config/SecurityConfig.java
+++ src/main/java/com/example/backend/global/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.example.backend.global.config;
 
 import com.example.backend.global.security.JwtAuthenticationFilter;
+import com.example.backend.global.security.JwtExceptionEntryPoint;
+import com.example.backend.global.security.JwtLogoutHandler;
+import com.example.backend.global.security.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,27 +22,38 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtLogoutHandler jwtLogoutHandler;
+    private final TokenService tokenService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors()//기본 cors 설정
+        http
+                .cors()//기본 cors 설정
                 .and()
-                .csrf().disable()
                 .formLogin().disable() //formLogin 인증 비활성화
                 .httpBasic().disable() //httpBasic 인증 비활성화
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 .and()
                 .authorizeRequests().antMatchers(
                         "/", "/login", "/kakaologin", "/join", "/kakaojoin", "/swagger-ui/**", "/api-docs/**")
                 .permitAll()
                 .anyRequest().authenticated();
 
-        http.addFilterAfter(
-                jwtAuthenticationFilter,
-                CorsFilter.class
-        );
+        http
+                .addFilterAfter(new JwtAuthenticationFilter(tokenService), CorsFilter.class);
+
+        http
+                .logout().permitAll()
+                .logoutUrl("/logout")
+                .deleteCookies("refreshToken") //로그아웃 후 쿠키 삭제
+                .addLogoutHandler(jwtLogoutHandler) //DB에서 RT 삭제
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
+
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(new JwtExceptionEntryPoint()) //예외처리
+                .and()
+                .csrf().disable();
 
         return http.build();
     }
---
+++ src/main/java/com/example/backend/global/security/JwtLogoutHandler.java
@@ -0,0 +1,51 @@
+package com.example.backend.global.security;
+
+import com.example.backend.domain.User;
+import com.example.backend.dto.ResponseDTO;
+import com.example.backend.global.exception.EntityNotExistsException;
+import com.example.backend.global.exception.EntityNotExistsExceptionType;
+import com.example.backend.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtLogoutHandler implements LogoutHandler {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        //시큐리티 컨텍스트에서 email 가져옴
+        String email = (String) authentication.getPrincipal();
+        //email로 DB에 있는 유저인지 확인
+        User findUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotExistsException(EntityNotExistsExceptionType.NOT_FOUND_USER));
+        //DB의 RT 삭제
+        findUser.deleteRefreshToken();
+
+        //set response
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        ResponseDTO<?> responseDTO = ResponseDTO.builder().success(true).message("로그아웃 되었습니다.").build();
+        //convert json
+        try {
+            ObjectMapper om = new ObjectMapper();
+            String result = om.writeValueAsString(responseDTO);
+            response.getWriter().write(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
