1b3627235b6aaab113dbf310696f3dc0835a6dac
ulzcq <syj9913@ynu.ac.kr>
2022-11-28 01:59:01 +0900

    feat: add HttpClient & RestTemplate Config

---
+++ src/main/java/com/example/backend/global/config/HttpClientConfig.java
@@ -0,0 +1,138 @@
+package com.example.backend.global.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * - Supports both HTTP and HTTPS
+ * - Uses a connection pool to re-use connections and save overhead of creating connections.
+ * - Has a custom connection keep-alive strategy (to apply a default keep-alive if one isn't specified)
+ * - Starts an idle connection monitor to continuously clean up stale connections.
+ */
+@Configuration
+@EnableScheduling
+@Slf4j
+public class HttpClientConfig {
+
+    private static final int CONNECT_TIMEOUT = 3000; //연결 시간
+    private static final int REQUEST_TIMEOUT = 5000; //요청 시간
+    private static final int SOCKET_TIMEOUT = 60000; //소켓 시간(데이터 기다리는 시간)
+    private static final int KEEP_ALIVE_TIME = 20 * 1000; //재사용되기전에 유지될 수 있는 시간
+    private static final int CLOSE_IDLE_CONNECTION_WAIT_TIME = 30;
+
+    private static final int MAX_TOTAL_CONNECTIONS = 50; //최대 커넥션 개수
+
+    //커넥션 풀 관리
+    @Bean
+    public PoolingHttpClientConnectionManager poolingConnectionManager(){
+        SSLContextBuilder builder = new SSLContextBuilder();
+        try {
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+            log.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage());
+        }
+
+        SSLConnectionSocketFactory sslsf = null;
+        try {
+            sslsf = new SSLConnectionSocketFactory(builder.build());
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            log.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
+        }
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+                .<ConnectionSocketFactory>create().register("https", sslsf)
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+
+        PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        poolingConnectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
+        return poolingConnectionManager;
+    }
+
+    //커넥션 대기시간 설정
+    @Bean
+    public ConnectionKeepAliveStrategy connectionKeepAliveStrategy() {
+        return new ConnectionKeepAliveStrategy() {
+            @Override
+            public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+                HeaderElementIterator it = new BasicHeaderElementIterator
+                        (response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+                while (it.hasNext()) {
+                    HeaderElement he = it.nextElement();
+                    String param = he.getName();
+                    String value = he.getValue();
+
+                    if (value != null && param.equalsIgnoreCase("timeout")) {
+                        return Long.parseLong(value) * 1000;
+                    }
+                }
+                return KEEP_ALIVE_TIME;
+            }
+        };
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient() {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(REQUEST_TIMEOUT)
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setSocketTimeout(SOCKET_TIMEOUT).build();
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(poolingConnectionManager())
+                .setKeepAliveStrategy(connectionKeepAliveStrategy())
+                .build();
+    }
+
+    //주기적으로 커넥션을 체크하며 해방시켜줌
+    @Bean
+    public Runnable idleConnectionMonitor(final PoolingHttpClientConnectionManager connectionManager) {
+        return new Runnable() {
+            @Override
+            @Scheduled(fixedDelay = 10000)
+            public void run() {
+                try {
+                    if (connectionManager != null) {
+                        log.trace("run IdleConnectionMonitor - Closing expired and idle connections...");
+                        connectionManager.closeExpiredConnections();
+                        connectionManager.closeIdleConnections(CLOSE_IDLE_CONNECTION_WAIT_TIME, TimeUnit.SECONDS);
+                    } else {
+                        log.trace("run IdleConnectionMonitor - Http Client Connection manager is not initialised");
+                    }
+                } catch (Exception e) {
+                    log.error("run IdleConnectionMonitor - Exception occurred. msg={}, e={}", e.getMessage(), e);
+                }
+            }
+        };
+    }
+
+}
