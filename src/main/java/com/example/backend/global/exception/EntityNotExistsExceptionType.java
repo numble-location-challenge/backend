96d73954583840818fd0fef224744a703cd70430
ulzcq <syj9913@ynu.ac.kr>
2022-11-28 01:23:35 +0900

    fix: NOT_FOUND_KAKAO_USER exception message

--- src/main/java/com/example/backend/global/exception/EntityNotExistsExceptionType.java
+++ src/main/java/com/example/backend/global/exception/EntityNotExistsExceptionType.java
@@ -7,7 +7,7 @@ import com.example.backend.global.exception.base.CustomExceptionType;
  */
 public enum EntityNotExistsExceptionType implements CustomExceptionType {
     NOT_FOUND_USER(-111, "존재하지 않는 'user' 입니다."),
-    NOT_FOUND_KAKAO_USER(-112, "회원가입 되지 않은 카카오 계정입니다."),
+    NOT_FOUND_KAKAO_USER(-112, "회원가입 되지 않은 카카오 계정입니다. /kakaojoin 으로 다시 요청하세요."),
     NOT_FOUND_FEED(-201, "존재하지 않는 'feed' 입니다."),
     NOT_FOUND_SOCIAL_LIST(-300, "작성된 게시글이 없습니다."),
     NOT_FOUND_SOCIAL(-301, "존재하지 않는 'social' 입니다."),
