400a793e9afaa38580c63eb490b98927886cc40d
Dsys1129 <dsys1129@gmail.com>
2022-11-27 19:16:39 +0900

    feat : add Feed ForbiddenExceptionType

--- src/main/java/com/example/backend/global/exception/ForbiddenExceptionType.java
+++ src/main/java/com/example/backend/global/exception/ForbiddenExceptionType.java
@@ -7,6 +7,8 @@ import com.example.backend.global.exception.base.CustomExceptionType;
  */
 public enum ForbiddenExceptionType implements CustomExceptionType {
     USER_UN_AUTHORIZED(-199, "권한이 없는 페이지입니다."),
+    NOT_AUTHORITY_DELETE_FEED(-202,"피드를 삭제할 권한이 없습니다."),
+    NOT_AUTHORITY_UPDATE_FEED(-203, "피드를 수정할 권한이 없습니다." ),
     NOT_AUTHORITY_UPDATE_COMMENT(-402, "댓글을 수정할 권한이 없습니다."),
     NOT_AUTHORITY_DELETE_COMMENT(-403, "댓글을 삭제할 권한이 없습니다.");
 
