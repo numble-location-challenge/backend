852d7cf385f2c23b2a7ca34a619bbc81a2d7064c
ulzcq <syj9913@ynu.ac.kr>
2022-11-28 19:03:56 +0900

    fix: sns login logic and add UserRepository method

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.repository;
 
 import com.example.backend.domain.User;
+import com.example.backend.domain.enumType.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.QueryHints;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);//이메일로 회원 조회
+    Optional<User> findByIdAndUserType(Long id, UserType userType);
 
     @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
     Optional<User> findReadOnlyByEmail(String email); //더티체킹 필요없이 조회만 필요할 때 사용
--- src/main/java/com/example/backend/service/user/SnsUserService.java
+++ src/main/java/com/example/backend/service/user/SnsUserService.java
@@ -1,10 +1,13 @@
 package com.example.backend.service.user;
 
+import com.example.backend.controller.TestKakaoToken;
 import com.example.backend.domain.User;
 import com.example.backend.domain.enumType.UserType;
 import com.example.backend.dto.login.SnsJoinRequestDTO;
 import com.example.backend.dto.user.SnsUserDTO;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 
 public interface SnsUserService {
