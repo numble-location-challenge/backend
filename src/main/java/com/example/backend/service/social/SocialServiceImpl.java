d6952c349de0f16cbd4e143edd11ba5e1d343e90
HongGuma <hongsu9849@gmail.com>
2022-11-30 15:12:39 +0900

    fix: fix the 'Like' function in a socialDetail
    모임 자세히 보기에 있는 좋아요 기능 수정
    - 좋아요 반영 잘 안되는 버그 수정

--- src/main/java/com/example/backend/service/social/SocialServiceImpl.java
+++ src/main/java/com/example/backend/service/social/SocialServiceImpl.java
@@ -136,13 +136,17 @@ public class SocialServiceImpl implements SocialService {
      */
     @Override
     public SocialLongDTO getSocialDetail(Long userId, Long postId) {
+        //게시글 찾기
         Social social = socialRepository.findById(postId)
                 .orElseThrow(() -> new EntityNotExistsException(EntityNotExistsExceptionType.NOT_FOUND_SOCIAL));
+
+        //dto만들기
         SocialLongDTO socialLongDTO = new SocialLongDTO(social);
 
+        //좋아요 여부 체크해서 반영하기
         socialLongDTO.updateLikeOrElse(likesRepository.existsByUserIdAndPostId(userId, postId));
 
-        return new SocialLongDTO(social);
+        return socialLongDTO;
     }
 
     /**
