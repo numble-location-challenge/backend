db9534622130d0f1a28c465cdb774dbbb90a3233
ulzcq <syj9913@ynu.ac.kr>
2022-12-01 03:59:31 +0900

    remove: SnsLoginRequestDTO

--- src/main/java/com/example/backend/service/login/LoginService.java
+++ src/main/java/com/example/backend/service/login/LoginService.java
@@ -3,7 +3,6 @@ package com.example.backend.service.login;
 import com.example.backend.domain.User;
 import com.example.backend.domain.enumType.UserType;
 import com.example.backend.dto.login.DefaultLoginRequestDTO;
-import com.example.backend.dto.login.SnsLoginRequestDTO;
 import com.example.backend.global.security.AuthToken;
 
 public interface LoginService {
